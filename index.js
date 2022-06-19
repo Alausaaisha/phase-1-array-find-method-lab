6b35919cf86608771118bcb4bc40bf249ebb5a06
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-19 19:39:30 +0100

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+//code your solution here
+function superbowlWin(Array) {
+    for (const record of Array) {
+        if (record.result === "W") {
+            return record.year
+        }
+    }
+}
+record.find(superbowlWin)
